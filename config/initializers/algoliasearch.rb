7a042a1bbf9ccdc9697d36b112edfb24fd4b471a
vahram87 <vahram.ts@gmail.com>
2025-09-17 15:59:28 +0400

    Did some fixes

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,4 +1,24 @@
-AlgoliaSearch.configuration = {
-  application_id: ENV.fetch("ALGOLIA_APPLICATION_ID"),
-  api_key: ENV.fetch("ALGOLIA_API_KEY")
-}
+# config/initializers/algoliasearch.rb
+
+# Don’t configure Algolia during tests
+if Rails.env.test?
+  Rails.logger.info("[Algolia] Skipping initialization in test")
+  return
+end
+
+app_id =
+  ENV["ALGOLIA_APPLICATION_ID"].presence ||
+  Rails.application.credentials.dig(:algolia, :application_id)
+
+api_key =
+  ENV["ALGOLIA_API_KEY"].presence ||
+  Rails.application.credentials.dig(:algolia, :api_key)
+
+if app_id.present? && api_key.present?
+  AlgoliaSearch.configuration = {
+    application_id: app_id,
+    api_key: api_key
+  }
+else
+  Rails.logger.warn("[Algolia] Credentials missing; not configuring")
+end
